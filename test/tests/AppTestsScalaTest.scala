daaf3588a3667c4d9f70ba36a4a80d9d56ac3d71
Michael Skogberg <michael.skogberg@outlook.com>
2017-02-03 17:54:33 +0200

    Remove unused code

--- test/tests/AppTestsScalaTest.scala
+++ test/tests/AppTestsScalaTest.scala
@@ -1,13 +1,12 @@
 package tests
 
-import com.malliina.app.{AppBuilder, AppComponents}
-import org.scalatest.FunSuite
+import com.malliina.app.AppComponents
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
 class TestAppSuite extends AppSuite(new AppComponents(_))
 
-class AppTestsScalaTest extends FunSuite with OneAppPerSuite2[AppComponents] with AppBuilder {
+class AppTestsScalaTest extends TestAppSuite {
   test("can make request") {
     val result = route(app, FakeRequest(GET, "/")).get
     assert(status(result) === 200)
